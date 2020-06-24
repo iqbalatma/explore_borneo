b3272c42aac76d4b9f259e04ab55b637430b680c
iqbalatma <iqbalatma@users.noreply.github.com>
2020-06-24 17:33:48 +0700

    membuat slider

--- app/src/main/java/com/example/exploreborneo/MainActivity.java
+++ app/src/main/java/com/example/exploreborneo/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.exploreborneo;
 
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    /*
+    NIM         : 10117124
+    Nama        : Iqbal Atma Muliawan
+    Kelas       : IF4
+    Matakuliah  : Aplikasi Komputasi Bergerak
+
+    24 Juni 2020    : Membuat main activity yang akan dijalankan ketika slider selesai
+ */
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PrefManager prefManager = new PrefManager(getApplicationContext());
+                prefManager.setFirstTimeLaunch(true);
+                startActivity(new Intent(MainActivity.this, SliderActivity.class));
+                finish();
+            }
+        });
+
     }
 }
